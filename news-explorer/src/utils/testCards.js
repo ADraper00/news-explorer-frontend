bebf50ab2c25eb3338a57ad0a2b939c4aeb74f6d
Ashly Draper <ashly_draper@msn.com>
2022-07-03 12:53:41 -0500

    stage 1

---
+++ news-explorer/src/utils/testCards.js
@@ -0,0 +1,67 @@
+export const cards = [
+  {
+    _id: 1,
+    cardkeyword: "Nature",
+    urlToImage:
+      "https://images.unsplash.com/photo-1587543648172-6c620eaff3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    publishedAt: "November 4, 2020",
+    title: "Everyone Needs a Special 'Sit Spot' in Nature",
+    content:
+      'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+    source: {
+      name: "treehugger",
+    },
+  },
+  {
+    _id: 2,
+    cardkeyword: "Nature",
+    urlToImage:
+      "https://images.unsplash.com/photo-1470770903676-69b98201ea1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=804",
+    publishedAt: "November 4, 2020",
+    title: "Nature makes you better",
+    content:
+      "We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.",
+    source: {
+      name: "National geographic",
+    },
+  },
+  {
+    _id: 3,
+    cardkeyword: "Yellowstone",
+    urlToImage:
+      "https://images.unsplash.com/photo-1578583705404-572695c5e32b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80",
+    publishedAt: "November 4, 2020",
+    title: "Nostalgic Photos of Tourists in U.S. National Parks",
+    content:
+      "Uri Løvevild Golman and Helle Løvevild Golman are National Geographic Explorers and conservation photographers who just completed a project and book they call their love letter to...",
+    source: {
+      name: "National parks traveler",
+    },
+  },
+  {
+    _id: 4,
+    cardkeyword: "Parks",
+    urlToImage:
+      "https://images.unsplash.com/photo-1603832857100-39ec58c0fe55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1317&q=80",
+    publishedAt: "November 4, 2020",
+    title: "Grand Teton Renews Historic Crest Trail",
+    content:
+      "The linking together of the Cascade and Death Canyon trails, at their heads, took place on October 1, 1933, and marked the first step in the realization of a plan whereby the hiker will be...",
+    source: {
+      name: "National geographic",
+    },
+  },
+  {
+    _id: 5,
+    cardkeyword: "Photography",
+    urlToImage:
+      "https://images.unsplash.com/photo-1526829013375-3f4ec736b0e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    publishedAt: "2022-05-17T00:42:03Z",
+    title: "Scientists Don't Know Why Polaris Is So Weird",
+    content:
+      "Humans have long relied on the starry sky to push into new frontiers, sail to the very edge of the world and find their way back home again. Even animals look to the stars to guide them.",
+    source: {
+      name: "National parks traveler",
+    },
+  },
+];
